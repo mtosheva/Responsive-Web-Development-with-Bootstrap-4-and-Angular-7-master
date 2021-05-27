3b5a320e8250ce66e6e9918094093c8d443d3a62
mtosheva <70014293+mtosheva@users.noreply.github.com>
2021-05-27 21:10:15 +0200

    added design chnages

--- src/app/core/app.component.ts
+++ src/app/core/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-portfolio-website';
+  backgroungImage: string = 'backgroungImage-home'
+  constructor(private router:Router){
+
+    this.router.events.subscribe((ev) => {
+      if (ev instanceof NavigationEnd) { 
+        this.checkClass();
+      }
+    });
+  }
+
+
+  
+  checkClass() {
+    if(this.router.url == "/home") {
+      this.backgroungImage = "backgroungImage-home"
+    } else if(this.router.url == "/contact") {
+      this.backgroungImage = "backgroungImage-contact"
+    }
+    else if(this.router.url == "/portfolio") {
+      this.backgroungImage = "backgroungImage-portfolio"
+    }
+  }
 }
