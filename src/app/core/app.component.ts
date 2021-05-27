cc86415ec4e097e6a119fe1ce0d86d1873c52ba5
mtosheva <70014293+mtosheva@users.noreply.github.com>
2021-05-27 21:37:29 +0200

    small design fixes

--- src/app/core/app.component.ts
+++ src/app/core/app.component.ts
@@ -9,6 +9,9 @@ import { NavigationEnd, Router } from '@angular/router';
 export class AppComponent {
   title = 'my-portfolio-website';
   backgroungImage: string = 'backgroungImage-home'
+  showHomepageText: boolean =true;
+  showContactText: boolean =false;
+  
   constructor(private router:Router){
 
     this.router.events.subscribe((ev) => {
@@ -23,11 +26,17 @@ export class AppComponent {
   checkClass() {
     if(this.router.url == "/home") {
       this.backgroungImage = "backgroungImage-home"
+      this.showHomepageText = true;
+      this.showContactText = false;
     } else if(this.router.url == "/contact") {
       this.backgroungImage = "backgroungImage-contact"
+      this.showHomepageText = false;
+      this.showContactText = true;
     }
     else if(this.router.url == "/portfolio") {
       this.backgroungImage = "backgroungImage-portfolio"
+      this.showHomepageText = false;
+      this.showContactText = false;
     }
   }
 }
